0ede43f3cfa48bdddf1a95a0234dc649a7e26675
rahulthakur212 <preetiagrahari134@gmail.com>
2023-02-03 15:42:03 +0545

    changes made

---
+++ warmup.js
@@ -0,0 +1,19 @@
+//bon-appetit 
+let result = 0;
+    for (let i=0;i<bill.length;i++)
+    {
+        if(i!==k)
+        {
+            result+=bill[i];
+        }
+    }
+    let pay= result/2;
+    if(b> pay)
+    {
+        console.log(b-pay);
+    }
+    else
+    {
+        return console.log("Bon Appetit");
+    }
+    go
