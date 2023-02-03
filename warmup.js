ceb0f3e9898809ce6aed81e9014761600afb90c4
Celestin25 <celestinhakorimana25@gmail.com>
2023-02-03 12:15:06 +0200

    Update made

--- warmup.js
+++ warmup.js
@@ -17,3 +17,27 @@ let result = 0;
         return console.log("Bon Appetit");
     }
 //
+//  Apple and Orange
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    // Write your code here
+    let k=0;
+    let j=0;
+    let i;
+    for(i=0;i<apples.length;i++)
+    {
+        if(a+apples[i]>=s && a+apples[i]<=t)
+        {
+            ++k;
+        }
+    }
+    for(i=0;i<oranges.length;i++)
+    {
+        if(b+oranges[i]>=s && b+oranges[i]<=t)
+        {
+            ++j;
+        }
+    }
+    console.log(k);
+    console.log(j);
+
+}
